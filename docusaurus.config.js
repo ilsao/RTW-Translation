3ba5dbe13e1b3514e9cc47f0af4e56eb4f2aeca6
Shuan <winniepooh.cs14@nycu.edu.tw>
2026-06-14 13:56:22 +0800

    fix: add my name to home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Ray Tracing in One Weekend 中文翻译',
-  tagline: 'By asciibase64',
+  tagline: 'By asciibase64 and Shuan',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
